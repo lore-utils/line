1d5f4bd0eb8329b541f8b8e51714a49753c4792b
Isaac Morneau <isaacmorneau@gmail.com>
2020-05-14 22:06:34 -0700

    options massage

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -12,33 +13,34 @@
 #include <unistd.h>
 
 static void print_help(void) {
-    printf("line - a line util to cleanly and sanely extract line(s)\n"
-           "Usage:\n"
-           "    line <OPTIONS>\n"
-           "Options:\n"
-           "    Lorum Ipsum\n"
-           "\n"
-           "    Dolor Sit Amet\n"
-           "\n"
-           "    -[-h]elp - this message\n"
-           "Notes:\n"
-           "    Lorem Ipsum.\n");
+    fprintf(stderr,
+        "line - a line util to cleanly and sanely extract line(s)\n"
+        "Usage:\n"
+        "    line <OPTIONS>\n"
+        "Options:\n"
+        "    Lorum Ipsum\n"
+        "\n"
+        "    Dolor Sit Amet\n"
+        "\n"
+        "    -[-h]elp - this message\n"
+        "Notes:\n"
+        "    Lorem Ipsum.\n");
 }
 
 //Line num is 1 indexed, nobody actually asks for line 0
-static void get_line(unsigned int line_num, const char *filename) {
+static void get_line(unsigned int line_num, const char * filename) {
     int fd = open(filename, O_RDONLY | O_CLOEXEC);
     if (fd == -1) {
         return;
     }
 
-    const long page_size = sysconf(_SC_PAGESIZE);
+    const int64_t page_size = sysconf(_SC_PAGESIZE);
     //TODO: Test how many pages are best
     const size_t buffer_size = page_size;
 
-    unsigned char *restrict buffer = malloc(buffer_size);
+    unsigned char * restrict buffer = malloc(buffer_size);
 
-    unsigned long long line_count = 1;
+    uint64_t line_count = 1;
 
     while (true) {
         ssize_t ret = read(fd, buffer, buffer_size);
@@ -52,18 +54,18 @@ static void get_line(unsigned int line_num, const char *filename) {
         if (ret == 0) {
             break;
         }
-        unsigned char *read_start = buffer;
-        size_t read_len = ret;
+        unsigned char * read_start = buffer;
+        size_t read_len            = ret;
 
         //Loop scope
         while (true) {
-            unsigned char *line_sep = memchr(read_start, '\n', read_len);
+            unsigned char * line_sep = memchr(read_start, '\n', read_len);
             size_t line_len;
             if (line_sep == NULL) {
                 //No separator found
                 if (line_count == line_num) {
                     line_len = read_len;
-                    printf("%.*s", (int) line_len, read_start);
+                    printf("%.*s", (int)line_len, read_start);
                 }
                 //Actually continue on the read loop, not the line loop
                 break;
@@ -71,7 +73,7 @@ static void get_line(unsigned int line_num, const char *filename) {
                 //Line len includes the \n
                 line_len = (line_sep - read_start) + 1;
                 if (line_count == line_num) {
-                    printf("%.*s", (int) line_len, read_start);
+                    printf("%.*s", (int)line_len, read_start);
                     //We only currently handle single line finding
                     return;
                 }
@@ -86,42 +88,62 @@ static void get_line(unsigned int line_num, const char *filename) {
     close(fd);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv) {
     if (argc == 1) {
         print_help();
         exit(EXIT_SUCCESS);
     }
 
-    unsigned int line_num = 0;
-    const char *filename = NULL;
+    uint64_t line_num = 0;
+
+    const char * filename = NULL;
 
     int c;
     while (1) {
         int option_index = 0;
 
         static struct option long_options[]
-                = {{"help", no_argument, 0, 'h'}, {"lines", required_argument, 0, 'l'},
-                        {"file", required_argument, 0, 'f'}, {0, 0, 0, 0}};
+            = {{"help", no_argument, 0, 'h'}, {"lines", required_argument, 0, 'l'},
+                {"file", required_argument, 0, 'f'}, {0, 0, 0, 0}};
 
-        c = getopt_long(argc, argv, "?hl:f:", long_options, &option_index);
-        if (c == -1)
+        c = getopt_long(argc, argv, "hl:f:", long_options, &option_index);
+
+        if (c == -1) {
+            for (int i = 1; i < argc - 1; ++i) {
+                char * endptr = 0;
+                line_num = strtoul(argv[i], &endptr, 0);
+                if (errno == ERANGE && line_num == ULLONG_MAX) {
+                    fprintf(stderr, "Line numbers must be between 1 and %lu\n", ULONG_MAX);
+                    print_help();
+                }
+            }
+            filename = argv[argc-1];
             break;
+        }
 
         switch (c) {
-            case 'l':
-                line_num = atoi(optarg);
-                break;
             case 'f':
                 filename = optarg;
                 break;
+            case 'l':
+            case ':': {
+                char * endptr = 0;
+                line_num = strtoul(optarg, &endptr, 0);
+                if (errno == ERANGE && line_num == ULLONG_MAX) {
+                    fprintf(stderr, "Line numbers must be between 1 and %lu\n", ULONG_MAX);
+                    print_help();
+                }
+                break;
+            }
             case 'h':
             case '?':
+                printf("huh?\n");
                 print_help();
                 exit(EXIT_SUCCESS);
         }
     }
     if (line_num == 0 || filename == NULL) {
-        printf("You need to specify both a line number and a filename\n");
+        fprintf(stderr, "You need to specify both a line number and a filename\n");
         print_help();
         exit(EXIT_SUCCESS);
     }
