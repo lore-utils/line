21da656758ccf722c0b69ff8afe06e3850fdb349
Isaac Morneau <isaacmorneau@gmail.com>
2020-05-16 16:51:26 -0700

    handle edgecases surrounding slow input and non-newline selected lines

--- src/lines.c
+++ src/lines.c
@@ -77,6 +77,9 @@ void buff_get_lines(int fd, condition_set_t * set) {
 
     uint64_t line_count = 1;
 
+    unsigned char * read_start = NULL;
+    size_t read_len = 0;
+    size_t line_len = 0;
     while (true) {
         ssize_t ret = read(fd, buffer, buffer_size);
         if (ret == -1) {
@@ -87,24 +90,25 @@ void buff_get_lines(int fd, condition_set_t * set) {
             return;
         }
         if (ret == 0) {
+            /*
+             * this is to handle the extremely rare edge case of having a final line that is both a match
+             * and not newline terminated. it cannot be handled in the null seperator check in case the
+             * program happens to be run in a mode with lots of small inputs as it will trigger the null case
+             * repeatedly causing early termination
+             */
+            if (line_len && line_match(set, line_count)) {
+                write(STDOUT_FILENO, read_start, line_len);
+            }
             break;
         }
-        unsigned char * read_start = buffer;
-        size_t read_len = ret;
+
+        read_start = buffer;
+        read_len = ret;
 
         while (true) {
             unsigned char * line_sep = memchr(read_start, '\n', read_len);
-            size_t line_len;
             if (line_sep == NULL) {
-                //No separator found
-                if (line_match(set, line_count)) {
-                    line_len = read_len;
-                    write(STDOUT_FILENO, read_start, line_len);
-                    if (!conditions_remaining(set)) {
-                        goto cleanup;
-                    }
-                }
-                //Actually continue on the read loop, not the line loop
+                line_len = read_len;
                 break;
             } else {
                 //Line len includes the \n
